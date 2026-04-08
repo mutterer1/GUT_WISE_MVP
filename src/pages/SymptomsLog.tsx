51038f22056e2c663773b92db98b0490faaf4c11
mutterer1 <muttereraaron@gmail.com>
2026-04-08 13:33:14 -0400

    Updated SymptomsLog.tsx

--- src/pages/SymptomsLog.tsx
+++ src/pages/SymptomsLog.tsx
@@ -397,7 +397,7 @@ export default function SymptomsLog() {
                         {log.triggers.map((trigger, idx) => (
                           <span
                             key={idx}
-                            className="inline-flex items-center rounded-full border-teal-700 bg-teal-50 px-2 py-1 text-xs text-gray-700"
+                            className="inline-flex items-center rounded-full border-gray-200 bg-gray-50 px-2 py-1 text-xs text-gray-900"
                           >
                             <AlertCircle className="mr-1 h-3 w-3" />
                             {trigger}
