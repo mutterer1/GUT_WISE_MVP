8b6a1cfe60c96115da594a86a96c804a68e21d06
mutterer1 <muttereraaron@gmail.com>
2026-04-08 01:58:20 -0400

    Updated Landing.tsx

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -66,7 +66,7 @@ export default function Landing() {
               </div>
 
               <h1 className="text-display-md sm:text-display-lg lg:text-5xl font-sora font-semibold text-neutral-text dark:text-dark-text mb-lg leading-tight">
-                Understand What Your{' '}
+                Understand What Your
                 <span className="block text-signal-500">Body<span className="block">Is Telling<span className="block text-discovery-500">You</span>
               </h1>
 
