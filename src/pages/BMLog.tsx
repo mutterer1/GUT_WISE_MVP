a59024eeafaa4bc2c97ec8983e5615934f0c9e84
mutterer1 <muttereraaron@gmail.com>
2026-04-08 12:40:38 -0400

    Updated BMLog.tsx

--- src/pages/BMLog.tsx
+++ src/pages/BMLog.tsx
@@ -445,7 +445,7 @@ function ToggleField({
         type="button"
         onClick={onToggle}
         className={`relative inline-flex h-6 w-11 items-center rounded-full transition-colors ${
-          active ? activeClass : 'bg-gray-700 dark:bg-dark-border'
+          active ? activeClass : 'bg-gray-700 dark:bg-gray-700'
         }`}
       >
         <span
