50afa69463b18079daf6ac33f824801bdfd9b20a
mutterer1 <muttereraaron@gmail.com>
2026-04-20 12:27:07 -0400

    Updated Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
 import { ArrowRight } from 'lucide-react';
-import { Button } from './Button';
+import Button from './Button';
 import { LogoFull } from './Logo';
 
 export default function Header() {
